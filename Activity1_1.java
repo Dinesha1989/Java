beb934ed749baec80e5e71c1178557f871775b95
Dinesha1989 <67994567+Dinesha1989@users.noreply.github.com>
2020-08-19 11:11:40 +0530

    Update Activity1_1.java

--- Activity1_1.java
+++ Activity1_1.java
@@ -5,7 +5,7 @@ public class Activity1_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//Initializing object
 		Car toyota = new Car();
 		
 		toyota.make = 2014;
